f13969240315866d434b18118309143d70116bec
BeckieMorton <beckam11@gmail.com>
2024-06-02 19:10:34 +1000

    2nd commit structure there added json file with available currencies which i need to filter through for the drop down boxes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,20 @@
 import styles from "./Search.module.css";
 
+import currencylist from "../../../public/data/currencylist.json";
+import { useState } from "react";
+
 export const Search = () => {
+  //need to use the currency api to get a list of currencies that are available and populate the dropdowns with this
+
+  const [currencies, setCurrencies] = useState();
+
+  //Your API Key: e4e59e71cba709378bb2bfbc
+  //signed up with tnl
+  //Example Request: https://v6.exchangerate-api.com/v6/e4e59e71cba709378bb2bfbc/latest/USD
+  //list of available currencies is in json file in public folder
+
+  setCurrencies(currencylist.filter((currency) => currency.CurrencyName));
+
   return (
     <div>
       <h2>Search</h2>
