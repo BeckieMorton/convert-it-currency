aebc9c4ed84f46f2f6687015ff503df5f4b3b8d3
BeckieMorton <beckam11@gmail.com>
2024-06-03 10:38:39 +1000

    got dropdown to display country and currency info using map

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,22 @@
 import styles from "./Search.module.css";
 
-import currencylist from "../../../public/data/currencylist.json";
+import currencylist from "../../data/currencylist.json";
 import { useState } from "react";
 
 export const Search = () => {
   //need to use the currency api to get a list of currencies that are available and populate the dropdowns with this
 
-  const [currencies, setCurrencies] = useState();
+  const countryCurrencyData = currencylist.map((item) => ({
+    country: item.Country,
+    currency: item.CurrencyName,
+  }));
+
+  console.log(`countries:`, countryCurrencyData);
 
   //Your API Key: e4e59e71cba709378bb2bfbc
   //signed up with tnl
   //Example Request: https://v6.exchangerate-api.com/v6/e4e59e71cba709378bb2bfbc/latest/USD
   //list of available currencies is in json file in public folder
-
-  setCurrencies(currencylist.filter((currency) => currency.CurrencyName));
 
   return (
     <div>
@@ -22,19 +25,21 @@ export const Search = () => {
         <div className={styles.searchBox}>
           <p>From</p>
           <select id="dropdown" name="dropdown">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-            <option value="option4">Option 4</option>
+            {countryCurrencyData.map((list, index) => (
+              <option value={list.country} key={index}>
+                {list.country} - {list.currency}
+              </option>
+            ))}
           </select>
         </div>
         <div className={styles.searchBox}>
           <p>To</p>
           <select id="dropdown" name="dropdown">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-            <option value="option4">Option 4</option>
+            {countryCurrencyData.map((list, index) => (
+              <option value={list.country} key={index}>
+                {list.country} - {list.currency}
+              </option>
+            ))}
           </select>
         </div>
       </div>
